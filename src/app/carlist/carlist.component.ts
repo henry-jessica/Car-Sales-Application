8301d32a13b564be81ce31584ac8c4af01c494f3
henry-jessica <s00214115@mail.itsligo.ie>
2022-03-21 23:38:54 +0000

    create car list component

--- src/app/carlist/carlist.component.ts
+++ src/app/carlist/carlist.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CarApiService } from '../services/car-api.service';
+import { ICar, Car } from '../interfaces/car'; 
 @Component({
   selector: 'app-carlist',
   templateUrl: './carlist.component.html',
-  styleUrls: ['./carlist.component.css']
+  styleUrls: ['./carlist.component.css'], 
+  providers:[CarApiService]
 })
 export class CarlistComponent implements OnInit {
 
-  constructor() { }
+  carData!: ICar[];
+  show !: boolean; 
+  
+  constructor(private _carAPIService:CarApiService) { }
 
   ngOnInit(): void {
+    this._carAPIService.getCarData().subscribe(carsData =>
+    { this.carData = carsData }); 
   }
-
+  addTheCar(make: string, model: string, year: string, imageURL: string): boolean{
+    let tempCar: ICar; 
+    tempCar = new Car(make, model, year, imageURL); 
+    this._carAPIService.addCarData(tempCar); 
+    return false; 
+  }
 }
